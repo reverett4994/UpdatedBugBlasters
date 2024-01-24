daa3de447261fc4d947b5d988011ff666abeb805
reverett4994 <reverett4994@gmail.com>
2024-01-23 23:47:05 -0500

    updatedEmailForm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,23 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
+
+    config.action_mailer.default_url_options = { host: 'https://bug-blasters-inc.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+
+  
 end
